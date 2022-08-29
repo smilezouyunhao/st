5c602faea1e0ea973444725d3803d46ccbc56684
smilezouyunhao <smilezouyunhao@163.com>
2022-08-29 23:03:42 +0800

    scrollback patch

--- config.h
+++ config.h
@@ -256,6 +256,10 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_9,           selectscheme,   {.i =  8} },
 	{ MODKEY,               XK_0,           nextscheme,     {.i = +1} },
 	{ MODKEY|ControlMask,   XK_0,           nextscheme,     {.i = -1} },
+	{ MODKEY,               XK_u,           kscrollup,   	{.i = -1} },
+	{ MODKEY,               XK_d,           kscrolldown,   	{.i = -1} },
+	//{ ShiftMask,		XK_Page_Up,	kscrollup,	{.i = -1} },
+	//{ ShiftMask,		XK_Page_Down,	kscrolldown,	{.i = -1} },
 };
 
 /*
